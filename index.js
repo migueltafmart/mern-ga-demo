2867f09bfb4a3786ccb1eb1a388d431da255a1d8
Miguel <migueltafmart@gmail.com>
2021-07-01 17:26:59 +0200

    IDK

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080;
 app.use("/api", apiRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/index.html"));
+  res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 app.listen(PORT, () =>
   console.log(`Server listening at http://localhost:${PORT}`)
