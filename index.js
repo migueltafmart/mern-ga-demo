b56e25178ff54f860aefcf3c03ddcf074658d184
Miguel <migueltafmart@gmail.com>
2021-07-01 17:30:29 +0200

    ISDK

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const PORT = process.env.PORT || 8080;
 
 app.use("/api", apiRouter);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/build/index.html"));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 app.listen(PORT, () =>
   console.log(`Server listening at http://localhost:${PORT}`)
 );
